67290d7dce9b5c2c402e385a3498dc83b1710ac8
FranciscoPires <frelvaspires@gmail.com>
2019-10-10 21:38:01 +0100

    warp system - by Diogo

---
+++ Assets/Scripts/Warping.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warping : MonoBehaviour
+{
+    [SerializeField] GameObject warpEntrance;
+    [SerializeField] GameObject Player;
+    //[SerializeField] Camera theCam;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine( Warp() );
+        }
+    }
+
+    IEnumerator Warp()
+    {
+        yield return new WaitForSeconds(0.2f);
+        
+        Player.transform.position = new Vector2(warpEntrance.transform.position.x,
+                warpEntrance.transform.position.y);
+
+        Camera.main.transform.position = new Vector3(0, -11, -5);
+
+    }
+}
