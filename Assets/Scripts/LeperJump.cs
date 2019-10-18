1fbd6b5948c0236ac2b9ff267124281738f41205
FRP7 <frelvaspires@gmail.com>
2019-10-18 17:02:47 +0100

    leper com transform

---
+++ Assets/Scripts/LeperJump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeperJump : MonoBehaviour
+{
+	public GameObject Leper;
+	public GameObject trigger;
+	public Animator AnimLeper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimLeper = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+	
+	 private void OnCollisionEnter2D(Collision2D collision)
+		 {
+				if (collision.gameObject.tag == "Player")
+				{
+				Debug.Log("Do something else here");
+				Leper.transform.position = trigger.transform.position;
+				//AnimLeper.Play("leper_jump");
+				}
+		}
+}
