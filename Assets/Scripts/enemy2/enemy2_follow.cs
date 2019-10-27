8d1e6d2cfe5a7d8fe6a07ac9fdf38d0bcbdf926f
FranciscoPires <frelvaspires@gmail.com>
2019-10-27 16:04:12 +0000

    enemy 2 & 3 flipam
    em principio, os enemy 2 e 3 vão flipar quando estiverem funcionais

---
+++ Assets/Scripts/enemy2/enemy2_follow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy2 : MonoBehaviour
+{
+    //flip
+    Vector3 enemy2Scale;
+    float enemy2ScaleX;
+    public float direction;
+    //
+    // Start is called before the first frame update
+    void Start()
+    {
+        //flip
+        enemy2Scale = transform.localScale;
+        enemy2ScaleX = enemy2Scale.x;
+        direction = transform.position.x;
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //flip
+        if (direction > transform.position.x) {
+            Debug.Log("Esta a mover para a esquerda");
+            enemy2Scale.x = enemy2ScaleX;
+        } else if (direction < transform.position.x) {
+            Debug.Log("Esta a mover para a direita");
+            enemy2Scale.x = -enemy2ScaleX;
+        }
+        direction = transform.position.x;
+        //
+    }
+}
---
+++ Assets/Scripts/enemy3_leper/enemy3_follow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy3_follow : MonoBehaviour
+{
+    //flip
+    Vector3 enemy3Scale;
+    float enemy3ScaleX;
+    public float direction;
+    //
+    // Start is called before the first frame update
+    void Start()
+    {
+        //flip
+        enemy3Scale = transform.localScale;
+        enemy3ScaleX = enemy3Scale.x;
+        direction = transform.position.x;
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //flip
+        if (direction > transform.position.x) {
+            Debug.Log("Esta a mover para a esquerda");
+            enemy3Scale.x = enemy3ScaleX;
+        } else if (direction < transform.position.x) {
+            Debug.Log("Esta a mover para a direita");
+            enemy3Scale.x = -enemy3ScaleX;
+        }
+        direction = transform.position.x;
+        //
+    }
+}
