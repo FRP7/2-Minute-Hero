a0f7eccd4a4035bb33cbaf77cf67b98547ddf989
FRP7 <frelvaspires@gmail.com>
2019-10-28 17:00:31 +0000

    enemy 2 trigger

--- Assets/Scripts/enemy2/enemy2_shoot.cs
+++ Assets/Scripts/enemy2/enemy2_shoot.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy2_shoot : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class enemy2_shoot : MonoBehaviour {
+
+	public Transform trigger1;
+    
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+	
     }
+	
+	public void Fire()
+	{
+		Debug.Log("Enemy 2 shoots");
+	}
 }
--- Assets/Scripts/enemy2/enemy2_trigger.cs
+++ Assets/Scripts/enemy2/enemy2_trigger.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class enemy2_trigger : MonoBehaviour
 {
+
+	public enemy2_shoot enemy2instance;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemy2instance = GameObject.Find("enemy2weapon").GetComponent<enemy2_shoot>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    public void OnCollisionEnter2D(Collision2D collision) {
+	// void OnCollisionStay2D(Collision2D collision) {
 
         if (collision.gameObject.name == "Player") {
-            Debug.Log("Enemy 2 trigou");
-        }
+           //Debug.Log("Enemy 2 trigou");
+		   enemy2instance.Fire();
+        } 
     }
 
 }
