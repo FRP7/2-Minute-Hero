e9c76c077bc92b6b417a45b3396712f97cd30e09
FranciscoPires <frelvaspires@gmail.com>
2019-10-30 16:17:34 +0000

    enemy 2 disparar (wip)
    o inimigo ja dispara, so falta as balas causarem dano e se destruirem

---
+++ Assets/Scripts/enemy2/enemy2_bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy2_bullet : MonoBehaviour
+{
+    float moveSpeed = 7f;
+
+    Rigidbody2D rb;
+
+    public GameObject target;
+    Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.Find("enemy2_trigger");
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+      //  Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.name == "Player") {
+            Debug.Log("Acertou!");
+            //Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/enemy2/enemy2_shoot.cs
+++ Assets/Scripts/enemy2/enemy2_shoot.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class enemy2_shoot : MonoBehaviour {
 
-	public Transform trigger1;
+    //public Vector2 trigger1;
+    public Transform trigger1;
+
+    public GameObject bullet;
+    public float speed = 100f;
+
+    public float fireRate;
+    public float nextFire;
     
     // Use this for initialization
     void Start()
     {
-
+        // bullet.GetComponent<Rigidbody2D>();
+        fireRate = 1f;
+        nextFire = Time.time;
     }
 
     // Update is called once per frame
@@ -21,5 +30,17 @@ public class enemy2_shoot : MonoBehaviour {
 	public void Fire()
 	{
 		Debug.Log("Enemy 2 shoots");
-	}
+       /*  GameObject instBullet = Instantiate(bullet, transform.position, Quaternion.identity)
+         as GameObject;
+         Rigidbody2D instBulletRigidbody = instBullet.GetComponent<Rigidbody2D>();
+        Vector2 shoot = (trigger1.position - bullet.transform.position).normalized;
+        instBulletRigidbody.AddForce(shoot * 500.0f);*/
+
+        if (Time.time > nextFire) {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+
+    }
+
 }
