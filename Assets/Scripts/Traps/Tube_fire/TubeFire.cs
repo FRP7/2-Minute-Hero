7365fad7130c89f7089adc3ba76420da7729ea3d
FranciscoPires <frelvaspires@gmail.com>
2020-02-19 18:04:54 +0000

    fire trap

---
+++ Assets/Scripts/Traps/Tube_fire/TubeFire.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubeFire : MonoBehaviour
+{
+    public enum states
+    {
+        on,
+        off
+    }
+    public states statemachine;
+    [SerializeField] private GameObject Fire;
+    [SerializeField] private float time;
+
+    private void Start() {
+        statemachine = states.off;
+    }
+
+    private void FixedUpdate() {
+        StateMachine();
+    }
+
+    public void StateMachine() {
+        switch(statemachine) {
+            case states.on:
+                Fire.SetActive(true);
+                StartCoroutine(ExecuteAfterTime(time));
+                break;
+            case states.off:
+                Fire.SetActive(false);
+                break;
+            default:
+                Fire.SetActive(false);
+                break;
+        }
+    }
+
+    IEnumerator ExecuteAfterTime(float time) {
+        yield return new WaitForSeconds(time);
+
+        statemachine = states.off;
+    }
+
+}
---
+++ Assets/Scripts/Traps/Tube_fire/TubeFireCol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubeFireCol : MonoBehaviour
+{
+    [SerializeField] private TubeFire tubefireinstance;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "playerweapon") {
+            tubefireinstance.GetComponent<TubeFire>();
+            tubefireinstance.statemachine = TubeFire.states.on;
+        }
+    }
+}
