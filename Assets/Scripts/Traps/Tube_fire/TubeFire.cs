cf8aa26c372a63169e61ea83cb6e8b7331b412e8
FranciscoPires <frelvaspires@gmail.com>
2020-02-19 19:24:31 +0000

    improvements na fire trap

--- Assets/Scripts/Traps/Tube_fire/TubeFire.cs
+++ Assets/Scripts/Traps/Tube_fire/TubeFire.cs
@@ -7,14 +7,17 @@ public class TubeFire : MonoBehaviour
     public enum states
     {
         on,
-        off
+        off,
+        idle
     }
     public states statemachine;
     [SerializeField] private GameObject Fire;
+    [SerializeField] private GameObject DamagedTube;
     [SerializeField] private float time;
+    public bool isIdle;
 
     private void Start() {
-        statemachine = states.off;
+        statemachine = states.idle;
     }
 
     private void FixedUpdate() {
@@ -25,13 +28,23 @@ public class TubeFire : MonoBehaviour
         switch(statemachine) {
             case states.on:
                 Fire.SetActive(true);
+                DamagedTube.SetActive(true);
                 StartCoroutine(ExecuteAfterTime(time));
+                isIdle = false;
+                break;
+            case states.idle:
+                Fire.SetActive(false);
+                DamagedTube.SetActive(false);
+                isIdle = true;
                 break;
             case states.off:
                 Fire.SetActive(false);
+                DamagedTube.SetActive(true);
+                isIdle = false;
                 break;
             default:
                 Fire.SetActive(false);
+                DamagedTube.SetActive(false);
                 break;
         }
     }
--- Assets/Scripts/Traps/Tube_fire/TubeFireCol.cs
+++ Assets/Scripts/Traps/Tube_fire/TubeFireCol.cs
@@ -7,7 +7,8 @@ public class TubeFireCol : MonoBehaviour
     [SerializeField] private TubeFire tubefireinstance;
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.tag == "playerweapon") {
+        if (collision.gameObject.tag == "playerweapon" 
+            && tubefireinstance.isIdle == true) {
             tubefireinstance.GetComponent<TubeFire>();
             tubefireinstance.statemachine = TubeFire.states.on;
         }
