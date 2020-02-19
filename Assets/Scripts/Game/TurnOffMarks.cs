c2d4d17c6c90b3e7558c5bf0e5f39761291b354f
FranciscoPires <frelvaspires@gmail.com>
2020-02-19 21:59:00 +0000

    marcadores colocados

---
+++ Assets/Scripts/Game/TurnOffMarks.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffMarks : MonoBehaviour
+{
+    private void Awake() {
+        DestroyEnemies();
+    }
+    public void DestroyEnemies() {
+        /*foreach (GameObject go in GameObject.FindGameObjectsWithTag("mark")) {
+            go.SetActive(false);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Wall")) {
+            go.SetActive(false);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Wall1")) {
+            go.SetActive(false);
+        }*/
+        /*foreach (GameObject go in GameObject.FindGameObjectsWithTag("mark")) {
+            foreach (SpriteRenderer renderer in go.GetComponentsInChildren(typeof(Renderer))) {
+                renderer.enabled = false;
+            }
+        }
+
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Wall")) {
+            foreach (SpriteRenderer renderer in go.GetComponentsInChildren(typeof(Renderer))) {
+                renderer.enabled = false;
+            }
+        }
+
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Wall1")) {
+            foreach (SpriteRenderer renderer in go.GetComponentsInChildren(typeof(Renderer))) {
+                renderer.enabled = false;
+            }
+        }*/
+
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("mark")) {
+            go.SetActive(false);
+        }
+    }
+}
