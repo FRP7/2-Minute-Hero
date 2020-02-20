b29bbcd1fad5bc48d281323b2bb7aee548be3a43
FranciscoPires <frelvaspires@gmail.com>
2020-02-20 16:39:45 +0000

    crusher (wip)

---
+++ Assets/Scripts/Traps/Maquina_crusher/Crusher_STM.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher_STM : MonoBehaviour
+{
+    [SerializeField] private Transform crusher;
+    [SerializeField] private Transform top;
+    [SerializeField] private Transform down;
+
+    public enum States {
+        top,
+        down
+    }
+
+    public States states;
+
+    private void FixedUpdate() {
+        STM();
+    }
+
+    private void STM() {
+        switch (states) {
+            case States.top:
+                crusher.transform.position = top.transform.position;
+                break;
+            case States.down:
+                crusher.transform.position = down.transform.position;
+                break;
+            default:
+                crusher.transform.position = down.transform.position;
+                break;
+        }
+    }
+}
