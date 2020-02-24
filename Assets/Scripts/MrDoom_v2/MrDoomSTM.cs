5be4807021f1efab786f64df9353f5dbc428d6a5
FranciscoPires <frelvaspires@gmail.com>
2020-02-24 16:34:19 +0000

    boss (wip), melhorias gerais

---
+++ Assets/Scripts/MrDoom_v2/MrDoomSTM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrDoomSTM : MonoBehaviour
+{
+    [SerializeField] private Health healthinstance;
+
+    private void FixedUpdate() {
+        healthinstance = GetComponent<Health>();
+        STM();
+    }
+
+    void STM() {
+       if(healthinstance.HP <= 2) {
+        }
+        if (healthinstance.HP > 2) {
+        }
+    }
+}
